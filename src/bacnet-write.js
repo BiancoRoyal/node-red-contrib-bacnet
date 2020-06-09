3c14d1544dcd4e61936db293c0b51d00603e42aa
Ekristoffe <aidasan@hotmail.fr>
2020-06-09 16:39:21 +0900

    Code review + Improvement
    Add ; at the end of the line.
    Change ' into " for strings.
    Check the settings ine the html file

--- src/bacnet-write.js
+++ src/bacnet-write.js
@@ -5,51 +5,54 @@
  All rights reserved.
  node-red-contrib-bacnet
  */
-'use strict'
+"use strict"
 
 module.exports = function (RED) {
-  const bacnetCore = require('./core/bacnet-core')
-  const BACnet = require('node-bacnet')
-  const _ = require('underscore')
+  const bacnetCore = require("./core/bacnet-core");
+  const BACnet = require("node-bacnet");
+  const _ = require("underscore");
 
   function BACnetWrite (config) {
-    RED.nodes.createNode(this, config)
+    RED.nodes.createNode(this, config);
 
-    this.name = config.name
-    this.objectType = parseInt(config.objectType)
-    this.valueTag = parseInt(config.valueTag)
-    this.valueValue = config.valueValue
-    this.propertyId = parseInt(config.propertyId)
-    this.priority = Math.min(Math.max((parseInt(config.priority) || 15), 1), 16)
+    this.name = config.name;
+    this.objectType = parseInt(config.objectType);
+    this.valueTag = parseInt(config.valueTag);
+    this.valueValue = config.valueValue;
+    this.propertyId = parseInt(config.propertyId);
+    this.priority = parseInt(config.priority);
 
-    this.multipleWrite = config.multipleWrite
+    this.multipleWrite = config.multipleWrite;
 
-    this.instance = RED.nodes.getNode(config.instance)
-    this.objectInstance = this.instance.instanceAddress || 0
+    this.instance = RED.nodes.getNode(config.instance);
+    this.objectInstance = this.instance.instanceAddress || 0;
 
-    this.device = RED.nodes.getNode(config.device)
-    this.deviceIPAddress = this.device.deviceAddress || '127.0.0.1' // IPv6 it is :: - but configure Node-RED too
+    this.device = RED.nodes.getNode(config.device);
+    this.deviceIPAddress = this.device.deviceAddress || "127.0.0.1"; // IPv6 it is :: - but configure Node-RED too
 
-    this.connector = RED.nodes.getNode(config.server)
+    this.connector = RED.nodes.getNode(config.server);
 
-    const node = this
+    const node = this;
 
-    node.status({ fill: 'green', shape: 'dot', text: 'active' })
+    node.status({ fill: "green", shape: "dot", text: "active" });
 
-    node.on('input', function (msg) {
+    node.on("input", function (msg) {
       if (!node.connector) {
-        node.error(new Error('Client Not Ready To Write'), msg)
-        return
+        node.error(new Error("Client Not Ready To Write"), msg);
+        return;
       }
+	  
+	  node.priority = (node.priority < 1 ? 16 : node.priority);
+	  node.priority = (node.priority > 16 ? 16 : node.priority);
 
-      const options = msg.payload.options || { priority: node.priority }
+      const options = msg.payload.options || { priority: node.priority };
 
       if (node.multipleWrite) {
-        bacnetCore.internalDebugLog('Multiple Write')
+        bacnetCore.internalDebugLog("Multiple Write");
 
         if (!msg.payload.values || !msg.payload.values[0].values) {
-          node.error(new Error('msg.payload.values missing or invalid array for multiple write'), msg)
-          return
+          node.error(new Error("msg.payload.values missing or invalid array for multiple write"), msg);
+          return;
         }
 
         msg.payload.values.forEach(function (item) {
@@ -59,12 +62,12 @@ module.exports = function (RED) {
               instance: parseInt(node.objectInstance)
             }
           }
-        })
+        });
 
         try {
-          bacnetCore.internalDebugLog('writePropertyMultiple msg.payload.values: ' + JSON.stringify(msg.payload.values))
+          bacnetCore.internalDebugLog("writePropertyMultiple msg.payload.values: " + JSON.stringify(msg.payload.values));
         } catch (e) {
-          bacnetCore.internalDebugLog('writePropertyMultiple error: ' + e)
+          bacnetCore.internalDebugLog("writePropertyMultiple error: " + e);
         }
 
         node.connector.client.writePropertyMultiple(
@@ -73,40 +76,40 @@ module.exports = function (RED) {
           options,
           function (err, value) {
             if (err) {
-              const translatedError = bacnetCore.translateErrorMessage(err)
-              bacnetCore.internalDebugLog(translatedError)
-              node.error(translatedError, msg)
+              const translatedError = bacnetCore.translateErrorMessage(err);
+              bacnetCore.internalDebugLog(translatedError);
+              node.error(translatedError, msg);
             } else {
-              msg.input = msg.payload
-              msg.payload = value
-              node.send(msg)
+              msg.input = msg.payload;
+              msg.payload = value;
+              node.send(msg);
             }
-          })
+          });
       } else {
-        bacnetCore.internalDebugLog('Write')
+        bacnetCore.internalDebugLog("Write");
 
         if (msg.payload.values && !msg.payload.values[0]) {
-          node.error(new Error('invalid msg.payload.values array for write'), msg)
-          return
+          node.error(new Error("invalid msg.payload.values array for write"), msg);
+          return;
         }
 
         const objectId = {
           type: node.objectType,
           instance: parseInt(node.objectInstance)
-        }
+        };
 
         const defaultValues = [{
           type: node.valueTag,
           value: node.valueValue
-        }]
+        }];
 
         try {
-          bacnetCore.internalDebugLog('readProperty default objectId: ' + JSON.stringify(objectId))
-          bacnetCore.internalDebugLog('writeProperty default values: ' + JSON.stringify(defaultValues))
-          bacnetCore.internalDebugLog('writeProperty msg.payload.values: ' + JSON.stringify(msg.payload.values))
-          bacnetCore.internalDebugLog('writeProperty node.propertyId: ' + node.propertyId)
+          bacnetCore.internalDebugLog("readProperty default objectId: " + JSON.stringify(objectId));
+          bacnetCore.internalDebugLog("writeProperty default values: " + JSON.stringify(defaultValues));
+          bacnetCore.internalDebugLog("writeProperty msg.payload.values: " + JSON.stringify(msg.payload.values));
+          bacnetCore.internalDebugLog("writeProperty node.propertyId: " + node.propertyId);
         } catch (e) {
-          bacnetCore.internalDebugLog('writeProperty error: ' + e)
+          bacnetCore.internalDebugLog("writeProperty error: " + e);
         }
 
         node.connector.client.writeProperty(
@@ -117,57 +120,57 @@ module.exports = function (RED) {
           options,
           function (err, value) {
             if (err) {
-              const translatedError = bacnetCore.translateErrorMessage(err)
-              bacnetCore.internalDebugLog(translatedError)
-              node.error(translatedError, msg)
+              const translatedError = bacnetCore.translateErrorMessage(err);
+              bacnetCore.internalDebugLog(translatedError);
+              node.error(translatedError, msg);
             } else {
-              msg.input = msg.payload
-              msg.payload = value
-              node.send(msg)
+              msg.input = msg.payload;
+              msg.payload = value;
+              node.send(msg);
             }
-          })
+          });
       }
-    })
+    });
   }
 
-  RED.nodes.registerType('BACnet-Write', BACnetWrite)
+  RED.nodes.registerType("BACnet-Write", BACnetWrite);
 
-  RED.httpAdmin.get('/bacnet/ApplicationTags', RED.auth.needsPermission('bacnet.CMD.write'), function (req, res) {
-    const typeList = BACnet.enum.ApplicationTag
-    const invertedTypeList = _.toArray(_.invert(typeList))
-    const resultTypeList = []
+  RED.httpAdmin.get("/bacnet/ApplicationTags", RED.auth.needsPermission("bacnet.CMD.write"), function (req, res) {
+    const typeList = BACnet.enum.ApplicationTag;
+    const invertedTypeList = _.toArray(_.invert(typeList));
+    const resultTypeList = [];
 
-    let typelistEntry
+    let typelistEntry;
     for (typelistEntry of invertedTypeList) {
-      resultTypeList.push({ typeValue: typeList[typelistEntry], label: typelistEntry })
+      resultTypeList.push({ typeValue: typeList[typelistEntry], label: typelistEntry });
     }
 
-    res.json(resultTypeList)
-  })
+    res.json(resultTypeList);
+  });
 
-  RED.httpAdmin.get('/bacnet/PropertyIds', RED.auth.needsPermission('bacnet.CMD.write'), function (req, res) {
-    const typeList = BACnet.enum.PropertyIdentifier
-    const invertedTypeList = _.toArray(_.invert(typeList))
-    const resultTypeList = []
+  RED.httpAdmin.get("/bacnet/PropertyIds", RED.auth.needsPermission("bacnet.CMD.write"), function (req, res) {
+    const typeList = BACnet.enum.PropertyIdentifier;
+    const invertedTypeList = _.toArray(_.invert(typeList));
+    const resultTypeList = [];
 
-    let typelistEntry
+    let typelistEntry;
     for (typelistEntry of invertedTypeList) {
-      resultTypeList.push({ typeValue: typeList[typelistEntry], label: typelistEntry })
+      resultTypeList.push({ typeValue: typeList[typelistEntry], label: typelistEntry });
     }
 
-    res.json(resultTypeList)
-  })
+    res.json(resultTypeList);
+  });
 
-  RED.httpAdmin.get('/bacnet/ObjectTypes', RED.auth.needsPermission('bacnet.CMD.write'), function (req, res) {
-    const typeList = BACnet.enum.ObjectType
-    const invertedTypeList = _.toArray(_.invert(typeList))
-    const resultTypeList = []
+  RED.httpAdmin.get("/bacnet/ObjectTypes", RED.auth.needsPermission("bacnet.CMD.write"), function (req, res) {
+    const typeList = BACnet.enum.ObjectType;
+    const invertedTypeList = _.toArray(_.invert(typeList));
+    const resultTypeList = [];
 
-    let typelistEntry
+    let typelistEntry;
     for (typelistEntry of invertedTypeList) {
-      resultTypeList.push({ typeValue: typeList[typelistEntry], label: typelistEntry })
+      resultTypeList.push({ typeValue: typeList[typelistEntry], label: typelistEntry });
     }
 
-    res.json(resultTypeList)
-  })
+    res.json(resultTypeList);
+  });
 }
